4412442da2274b7cc5d80c68f19dc901f488ee0f
juansechaux <juansechaux@hotmail.com>
2023-07-27 17:16:22 -0700

    prueba11

--- _printf.c
+++ _printf.c
@@ -19,7 +19,11 @@ int _printf(const char *format, ...)
 		{NULL, NULL}
 	};
 	if (str == NULL || format == NULL)
+	{
+		va_end(arguments);
+		free(str);
 		return (0);
+	}
 	str[0] = '\0';
 	va_start(arguments, format);
 	while (format[i] != '\0')
